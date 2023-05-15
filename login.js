5d583d818b89787e3d9f28736597dfdcb9db0d70
nave cohen <nave.cohen@e.braude.ac.il>
2023-05-15 23:46:23 +0300

    first upload

---
+++ login.js
@@ -0,0 +1,13 @@
+function login() {
+    // Get the values from input fields
+    var username = document.getElementById('user-name').value;
+    var password = document.getElementById('user-pass').value;
+
+    // Call the Python function with the parameters
+    google.colab.kernel.invokeFunction('db.login', [username, password, true], {}).then(function (result) {
+        var res = result.data['text/plain'];
+        if (res == 'False') {
+            document.getElementById('err-msg').innerText = "Username/Password are incorect";
+        }
+    });
+}
