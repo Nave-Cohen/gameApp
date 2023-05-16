1fd60159025eb8379b7287cab65b018dbea1c09b
Nave-Cohen <117146523+Nave-Cohen@users.noreply.github.com>
2023-05-16 16:03:26 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,11 @@
-google.colab.kernel.invokeFunction('db.iAdmin', [], {}).then(function (result) {
+(async function(){
+  var result = await google.colab.kernel.invokeFunction('db.iAdmin', [], {})
   var res = result.data['text/plain'];
+  document.getElementById('spinner').remove()
   if (res == 'True') {
     document.getElementById('admin-btn').classList.remove('d-none')
+    document.getElementById('start-btn').classList.remove('d-none')
+  } else {
+    document.getElementById('start-btn').classList.remove('d-none')
   }
-}
-).then(() => { document.getElementById('spinner').classList.add('d-none') })
+})()
