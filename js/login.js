e4473c7ca066cb8ac9e664306d27bc6f10679615
Nave-Cohen <117146523+Nave-Cohen@users.noreply.github.com>
2023-05-16 04:09:59 +0300

    Update login.js

--- js/login.js
+++ js/login.js
@@ -10,7 +10,7 @@ function login() {
             document.getElementById('err-msg').innerText = "Username/Password are incorect";
         }
         else {
-            console.log("wrong")
+            window.location.href = 'https://raw.githubusercontent.com/Nave-Cohen/gameApp/main/html/second.html'
         }
     });
 }
