1f6431e42a5eb4b3667eadb17a39be4436bc8a24
Nave-Cohen <117146523+Nave-Cohen@users.noreply.github.com>
2023-05-17 17:24:58 +0300

    Update utils.js

--- js/utils.js
+++ js/utils.js
@@ -1,6 +1,7 @@
 async function call(function_name) {
     args = [...arguments].splice(1)
-    var res = await google.colab.kernel.invokeFunction(function_name, args, {})
+    var res = await google.colab.kernel.invokeFunction(function_name, args, {})    
+    if (res == null){return}
     if (res.data['text/plain'] == 'True') { return true }
     if (res.data['text/plain'] == 'False') { return false }
     return res.data
