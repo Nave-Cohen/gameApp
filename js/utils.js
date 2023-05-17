c236ea829f829cb4129d55a1f7de016f84a21d43
Nave-Cohen <117146523+Nave-Cohen@users.noreply.github.com>
2023-05-17 13:26:48 +0300

    Create utils.js

---
+++ js/utils.js
@@ -0,0 +1,10 @@
+async function call(function_name) {
+    args = [...arguments].splice(1)
+    var res = await google.colab.kernel.invokeFunction(function_name, args, {})
+    if (res.data['text/plain'] == 'True') { return true }
+    if (res.data['text/plain'] == 'False') { return false }
+    return res.data
+}
+function view_page() {
+    google.colab.kernel.invokeFunction('show_page', [...arguments], {})
+}
