5dd5719ae4418708093a0efbca6f5788b526422a
Nave-Cohen <117146523+Nave-Cohen@users.noreply.github.com>
2023-05-17 21:45:32 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
-
+async function show_question() {
+  var question = await call('generate_question')
+  console.log(question)
+  view_page('question', {
+    'title': question.title, 'question1': question.questions["answers"][0],
+    'question2': question.questions["answers"][1],
+    'question3': question.questions["answers"][2],
+    'question4': question.questions["answers"][3]
+  })
+}
