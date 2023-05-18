e79b1c9222a4f5c65999c0bf60b2151f88faabd5
Nave-Cohen <117146523+Nave-Cohen@users.noreply.github.com>
2023-05-18 21:24:18 +0300

    Update utils.js

--- js/utils.js
+++ js/utils.js
@@ -1,21 +1,18 @@
 async function call(function_name) {
-  var outputObject;
   args = [...arguments].splice(1)
   var res = await google.colab.kernel.invokeFunction(function_name, args, {})
   if (res == null) { return }
   const outputString = res.data['text/plain'].trim();
   if (outputString === 'True') {
-    outputObject = true;
+    return true;
   } else if (outputString === 'False') {
-    outputObject = false;
-  } else {
-    try {
-      outputObject = JSON.parse(outputString.split("'").join(''));
-    } catch { 
-      return outputString;
-    }
+    return false;
   }
-  return outputObject;
+  return outputString;
+}
+async function callJson(function_name, ...args) {
+    var res = await google.colab.kernel.invokeFunction(function_name, args, {})
+    return res.data["application/json"]
 }
 function view_page() {
   google.colab.kernel.invokeFunction('show_page', [...arguments], {})
