a58989737ac0949a450730c6cdf2ef10271b7e7b
Nave-Cohen <117146523+Nave-Cohen@users.noreply.github.com>
2023-05-18 21:24:36 +0300

    Update question.js

--- js/question.js
+++ js/question.js
@@ -1,9 +1,36 @@
-function checkAnswer(button) {
-  var answer = button.innerText;
+var question
+(async function gen() {
+  var question_div = document.getElementById('questionDiv');
+  question = await callJson('generate_question');
 
-  if (answer === "Paris") {
-    alert("Correct answer! Paris is the capital of France.");
-  } else {
-    alert("Sorry, that is incorrect. Please try again.");
+  const label = document.createElement('label');
+  label.setAttribute('for', 'questionDiv');
+  label.textContent = question.title;
+  question_div.appendChild(label);
+  for (i in [0, 1, 2, 3]) {
+    var button = document.createElement('button');
+    button.setAttribute('type', 'button');
+    button.setAttribute('id', `Option${i}`)
+    button.setAttribute('onclick', `checkAnswer(${button.id},${question.correct})`);
+    button.textContent = `${question.answers[i]}`;
+    question_div.appendChild(button);
   }
+})();
+
+async function checkAnswer(btn, correct) {
+  var correct_btn = document.getElementById('Option' + correct)
+  if (btn == correct_btn) {
+    btn.style = "background-color:green";
+    await call('submit_question',question['title'],question['answers'][question['correct']],true);
+  }
+  else {
+    btn.style = "background-color:red";
+    correct_btn.style = "background-color:green";
+    await call('submit_question',question['title'],question['answers'][question['correct']],false);
+  }
+  await sleep(4000)
+  view_page('question')
+}
+function sleep (time) {
+  return new Promise((resolve) => setTimeout(resolve, time));
 }
