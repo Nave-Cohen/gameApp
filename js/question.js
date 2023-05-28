1e9bb2b883abb74ec33630f3cb5e15149b25306c
Nave-Cohen <117146523+Nave-Cohen@users.noreply.github.com>
2023-05-28 15:41:45 +0300

    Update question.js

--- js/question.js
+++ js/question.js
@@ -1,48 +1,42 @@
-var question
+var question;
+
 (async function gen() {
   var question_div = document.getElementById('questionDiv');
   question = await callJson('generate_question');
-  if (question == null){
-    call('view_timeline')
+  if (question == null) {
+    call('view_timeline');
   }
-  const label = document.createElement('label');
-  label.setAttribute('for', 'questionDiv');
-  label.textContent = question.title;
-  question_div.appendChild(label);
-  for (let i = 1; i <= 4;i++) {
-    var button = document.createElement('button');
-    button.setAttribute('type', 'button');
-    button.setAttribute('id', `Option${i}`)
-    button.setAttribute('onclick', `checkAnswer(${button.id},${question.correct})`);
-    button.textContent = `${question["Option"+i]}`;
-    question_div.appendChild(button);
+  question_div.innerHTML = `
+    <label for="questionDiv">${question.title}</label>
+  `;
+  for (let i = 1; i <= 4; i++) {
+    question_div.innerHTML += `
+      <button type="button" id="Option${i}" onclick="checkAnswer(this, ${question.correct})">${question["Option" + i]}</button>
+    `;
   }
 })();
 
 async function checkAnswer(btn, correct) {
-  var correct_btn = document.getElementById('Option' + correct)
-  if (btn == correct_btn) {
-    btn.style = "background-color:green";
-    await call('submit_question', question['title'], question['Option'+correct], true);
+  var correct_btn = document.getElementById('Option' + correct);
+  if (btn === correct_btn.id) {
+    btn.style.backgroundColor = "green";
+    await call('submit_question', question.title, question['Option' + correct], true);
     await sleep(1000);
     view_page('answer', { "answer": 'Your answer is correct!' });
     await sleep(1000);
-  }
-  else {
-    btn.style = "background-color:red";
-    correct_btn.style = "background-color:green";
-    await call('submit_question', question['title'], question['Option'+correct], false);
+  } else {
+    btn.style.backgroundColor = "red";
+    correct_btn.style.backgroundColor = "green";
+    await call('submit_question', question.title, question['Option' + correct], false);
     await sleep(1000);
     view_page('answer', { "answer": 'Your answer is wrong!' });
     await sleep(1000);
   }
-
-    
 }
-function sleep (time) {
+
+function sleep(time) {
   return new Promise((resolve) => setTimeout(resolve, time));
 }
-
 
 (async function updateProgressBar() {
   var prog = await call('progress');
@@ -50,11 +44,12 @@ function sleep (time) {
   progressBar.style.width = prog + "%";
   progressBar.setAttribute("aria-valuenow", prog);
 })();
-  
+
 function logout() {
-  view_page('login')
-  call('db.logout')
+  view_page('login');
+  call('db.logout');
 }
+
 function end() {
-  call('view_timeline')
+  call('view_timeline');
 }
