b91be02c1c8ff83225b0a801e9d2735df3ee299b
Nave-Cohen <117146523+Nave-Cohen@users.noreply.github.com>
2023-05-31 19:10:19 +0300

    Update add-question.js

--- js/add-question.js
+++ js/add-question.js
@@ -13,5 +13,12 @@ async function addQuestion() {
   var data = { 'title': question, 'Option1': opt1, 'Option2': opt2, 'Option3': opt3, 'Option4': opt4, 'correct': correct };
 
   var result = await call('db.addQuest', data)
-  console.log(result)
+  if (result == true) {
+    var msg = document.createElement("p");
+    msg.id = "msg";
+    msg.className = "text-center";
+    msg.style.color = "green";
+    msg.textContent = "Answer added successfully";
+    document.querySelector('#main-container').appendChild(msg);
+  }
 }
