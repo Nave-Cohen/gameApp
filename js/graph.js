fe3e265e665d6aea830cb91de7e6ab5937a5242b
Nave-Cohen <117146523+Nave-Cohen@users.noreply.github.com>
2023-05-31 21:15:31 +0300

    Update graph.js

--- js/graph.js
+++ js/graph.js
@@ -23,3 +23,14 @@
   });
 
 })();
+
+async function back() {
+  await call('clear')
+  var isAdmin = await call('db.isAdmin')
+    var username = await call('db.getUserName')
+    options = {
+      admin: isAdmin,
+      name: username
+    }
+    view_page('main', options)
+}
