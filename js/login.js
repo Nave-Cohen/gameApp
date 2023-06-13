0de0899a240dc5b7a0d7c94e79f5e3569f3833a3
Nave-Cohen <117146523+Nave-Cohen@users.noreply.github.com>
2023-06-13 14:29:18 +0300

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,17 +1,14 @@
 async function login() {
-  var username = document.getElementById('user-name').value;
-  var password = document.getElementById('user-pass').value;
+  var username = document.getElementById("user-name").value;
+  var password = document.getElementById("user-pass").value;
 
-  var result = await call('db.login', username, password)
+  var result = await call("db.login", username, password);
   if (!result) {
-    document.getElementById('err-msg').innerText = "Username/Password are incorrect";
-  }
-  else {
-    var isAdmin = await call('db.isAdmin')
-    options = {
-      admin: isAdmin,
-      name: username
-    }
-    view_page('main', options)
+    document.getElementById("err-msg").innerText =
+      "Username/Password are incorrect";
+  } else {
+    var user = await callJson("db.getUser");
+    console.log(user);
+    view_page("main", user);
   }
 }
