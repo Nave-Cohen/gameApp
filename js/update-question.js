edc1d8ca5957fc0ec7d0ff6ea1d4fa2574038388
Nave-Cohen <117146523+Nave-Cohen@users.noreply.github.com>
2023-06-13 14:30:52 +0300

    Update update-question.js

--- js/update-question.js
+++ js/update-question.js
@@ -109,11 +109,7 @@ var value = element.value
 }
 async function back() {
   await call('clear')
-  var isAdmin = await call('db.isAdmin')
-  var username = await call('db.getUserName')
-  options = {
-    admin: isAdmin,
-    name: username
-  }
-  view_page('main', options)
+    var user = await callJson("db.getUser");
+    console.log(user);
+    view_page("main", user);
 }
